dfd0ba8361f1fd93cb860ed5240285cf8ba4040f
Avichal Kumar <Avichalllkumar@gmail.com>
2025-07-31 14:50:11 +0530

    changed module

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+
+
+export default async function Navbar() {
+
+
+
+    return(
+        <>
+            <div>
+                <header>
+                    <div className="bg-black w-[1440px] h-[61px] flex items-center gap-10" >
+                        
+                        <div className="text-white">+916202271745</div>
+                        <div className="text-white">corporate@evox.com</div>
+
+                    </div>
+                </header>
+
+            </div>
+
+        </>
+    )
+    
+}
